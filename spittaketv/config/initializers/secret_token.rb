44b5b9d7da996629a50dfc0271dfac47f42c0d3f
Trevor Burton <trevor.burton@quick.tv>
2011-02-23 22:25:07 +0000

    added the bare rails webapp

---
+++ spittaketv/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spittaketv::Application.config.secret_token = '47b3ef038fe4234bd8e9e230408b4c7ec52da9bcc4848848e7e662ca2d7609c816599a0e1d15cc1b2244eb39dcf1b91379eaf38908c967db23fb1c46fa9817c6'
